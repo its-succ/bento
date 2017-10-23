c04e70a1cf419fcdbdbebd329f7671bbf4dd7209
SAKAI Yoshihito <y-sakai@esm.co.jp>
2017-10-23 09:17:47 +0900

    GAEアプリ化

--- src/main/java/jp/co/esm/bento/web/BentoWebApplication.java
+++ src/main/java/jp/co/esm/bento/web/BentoWebApplication.java
@@ -2,9 +2,16 @@ package jp.co.esm.bento.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BentoWebApplication {
+public class BentoWebApplication extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(BentoWebApplication.class);
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(BentoWebApplication.class, args);
