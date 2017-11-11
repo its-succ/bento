e495e5d30d6b76291fed3bbfc2288edacab2fca6
SAKAI Yoshihito <y-sakai@esm.co.jp>
2017-11-11 12:33:00 +0900

    Quasar Frameworkで作ってみた

---
+++ vue/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <!-- Don't drop "q-app" class -->
+  <div id="q-app">
+    <q-layout>
+      <q-toolbar slot="header">
+        <q-toolbar-title class="text-bold">ぐるめし</q-toolbar-title>
+      </q-toolbar>
+      <q-tabs slot="navigation">
+        <q-route-tab slot="title" replace to="/history" class="text-bold">履歴</q-route-tab>
+        <q-route-tab slot="title" replace to="/order" class="text-bold">注文</q-route-tab>
+      </q-tabs>
+      <router-view id="content-view"></router-view>
+    </q-layout>
+  </div>
+</template>
+
+<script>
+/*
+ * Root component
+ */
+import {
+  QLayout,
+  QToolbar,
+  QToolbarTitle,
+  QTabs,
+  QRouteTab
+} from 'quasar'
+
+export default {
+  components: {
+    QLayout,
+    QToolbar,
+    QToolbarTitle,
+    QTabs,
+    QRouteTab
+  }
+}
+</script>
+
+<style>
+#content-view {
+  padding: 8px
+}
+</style>
---
+++ vue/src/components/History.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="history">
+    <table class="q-table bordered vertical-separator striped-odd">
+      <caption class="text-left">{{week}} 〜
+      </caption>
+      <thead class="bg-primary text-white">
+        <tr>
+          <th colspan="2">日付</th>
+          <th>おかず</th>
+          <th>ごはん</th>
+          <th>味噌汁</th>
+          <th>金額</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="order in orders" :key="order.date">
+          <td>{{order.date}}</td>
+          <td>{{order.dow}}</td>
+          <td>{{order.main}}</td>
+          <td>{{order.rice}}</td>
+          <td>{{order.soup}}</td>
+          <td class="text-right">¥{{order.price}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'history',
+  data () {
+    return {
+      week: '2017-11-01',
+      orders: [
+        { date: '11/1', dow: '月', main: '愛', rice: 'ふつう', soup: true, price: 300 },
+        { date: '11/2', dow: '火', main: 'ゆうき', rice: 'ふつう', soup: true, price: 360 }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
