fc73b5508aeb533a32b0d3bba8889f8c45d27532
Miho Nakamoto <miho.nakamoto@esm.co.jp>
2019-06-07 16:30:33 +0900

    homeを設定した

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import SignIn from "@/views/SignIn.vue";
-// import Home from "@/views/Home.vue";
+import Home from "@/views/Home.vue";
 
 Vue.use(Router);
 
@@ -15,13 +15,9 @@ export default new Router({
       component: SignIn
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: "/home",
+      name: "home",
+      component: Home
     }
   ]
 });
