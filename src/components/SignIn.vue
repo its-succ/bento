35f67c3e49f4a880f47f7ed09fc27cf9c9a38b91
Miho Nakamoto <miho.nakamoto@esm.co.jp>
2019-06-07 15:06:48 +0900

    GoogleSignInを追加した

---
+++ src/components/SignIn.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="signin">
+    <a @click="signIn" class="button--green">signIn</a>
+  </div>
+</template>
+
+
+<script>
+import firebase from "firebase";
+
+export default {
+  methods: {
+    signIn: function() {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithRedirect(provider);
+    }
+  }
+};
+</script>
