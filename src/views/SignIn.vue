b39e382f30bd694a15ed9253d58e8764a519928e
Miho Nakamoto <miho.nakamoto@esm.co.jp>
2019-06-07 15:41:44 +0900

    最初の画面をSignIn.vueにした

---
+++ src/views/SignIn.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>本社弁当注文</h1>
+    <sign-in />
+  </div>
+</template>
+
+<script>
+import SignIn from "@/components/SignIn.vue";
+
+export default {
+  name: "home",
+  components: {
+    "sign-in" : SignIn
+  }
+};
+</script>
