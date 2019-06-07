cdf89f64f100d607d3b32a5f008d9e8ec0e76b7a
Miho Nakamoto <miho.nakamoto@esm.co.jp>
2019-06-07 15:06:00 +0900

    main.jsにfirebaseの設定を追加した

--- src/main.js
+++ src/main.js
@@ -2,6 +2,19 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_jueY2J6SGNwOrIk45A5CWLZqNlEOcC8",
+  authDomain: "bento-184202.firebaseapp.com",
+  databaseURL: "https://bento-184202.firebaseio.com",
+  projectId: "bento-184202",
+  storageBucket: "bento-184202.appspot.com",
+  messagingSenderId: "599875423654",
+  appId: "1:599875423654:web:5ec328cb63bd7472"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false;
 
