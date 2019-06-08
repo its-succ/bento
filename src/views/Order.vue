ed452d3dd2e1ceecdaef574814457b17c95e195b
SAKAI Yoshihito <y-sakai@esm.co.jp>
2019-06-08 10:38:24 +0900

    来週を表示

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -1,8 +1,50 @@
 <template>
-    
+  <div class="home">
+    <table>
+      <thead>
+        <tr>
+          <th>日付</th>
+          <th>おかず</th>
+          <th>ごはん</th>
+          <th>味噌汁</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="order in orders" :key="order.date">
+          <td>{{ order.date }}</td>
+          <td>{{ order.menu }}</td>
+          <td>{{ order.rice }}</td>
+          <td>{{ order.miso }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
+
 <script>
+import firebase from "firebase";
+import { edgeOfWeek, formatDate } from "@/util";
+import { addWeeks, format, eachDay } from 'date-fns';
+import ja from 'date-fns/locale/ja'
+
 export default {
-  name: "order"
+  name: "order",
+  data() {
+    return {
+      orders: [],
+    }
+  },
+  async mounted() {
+    const next = addWeeks(new Date(), 1);
+    const edge = edgeOfWeek(next);
+    this.orders = eachDay(edge.start, edge.end).map(date => {
+      return {
+        date: format(date, "M/D(dd)", { locale: ja }),
+        menu: "",
+        rice: "",
+        miso: false,
+      };
+    });
+  }
 }
 </script>
